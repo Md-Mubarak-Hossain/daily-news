c45331b3a151ab31b97d9d1c1f12b9cfa62e2710
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-09-04 10:38:00 +0600

    no data available update

--- js/dailyNews.js
+++ js/dailyNews.js
@@ -77,55 +77,73 @@ const displayNews = (newsDetail, newsCategoryName) => {
         newsDiv.classList.add('card');
         newsDiv.classList.add('mb-3');
         newsDiv.classList.add('rounded-5');
+        newsDiv.classList.add('d-none');
         if (`${eachNews.author.name}` === "null" || `${eachNews.total_view}` === "null") {
             console.log('null');
-        }
+            const newsDiv2 = document.createElement('div');
+            newsDiv2.classList.add('card');
+            newsDiv2.classList.add('mb-3');
+            newsDiv2.classList.add('py-5');
+            newsDiv2.classList.add('text-center');
+            newsDiv2.classList.add('fw-bold');
+            newsDiv2.classList.add('fs-3');
+            newsDiv2.classList.add('text-danger');
 
-        newsDiv.innerHTML = `   
-        <div onclick="loadNewsDetails('${eachNews.title}','${eachNews.author.name}','${eachNews.details.slice(0, 400)}')"
-        class="row ">
-        <div class="col-md-4 p-5">
-            <img height=200px; src="${eachNews.thumbnail_url}" class="img-fluid rounded-start " alt="...">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body py-5">
-                <h3 class="card-title">${eachNews.title}</h3>
-                <p class="card-text">${eachNews.details.slice(0, 300)}<strong data-bs-toggle="tooltip"
-                        data-bs-placement="top" data-bs-title="Tooltip on top" title="more read click">
-                        ...
-                    </strong>
-                </p>
-    
-                <div class="text-muted d-md-flex d-lg-flex text-justify justify-content-between pt-md-5 mt-md-5">
-                    <div class="d-flex">
-                        <img height=45px; class="rounded-circle mx-2" src="${eachNews.author.img}">
-                        <div style="height:40px">
-                            <p style="height:50%; margin:0 2px" class="fw-bold text-dark">${eachNews.author.name}</p>
-                            <p style="height:50%; margin:0 2px">${eachNews.author.published_date}
-                            </p>
-                        </div>
+            newsDiv2.classList.add('rounded-5');
+
+            newsDiv2.innerHTML = `<p>No data available</p>`;
+            newsCard.appendChild(newsDiv2); ''
+        }
+        else {
+            newsDiv.classList.remove('d-none');
+            newsDiv.innerHTML = `   
+    <div onclick="loadNewsDetails('${eachNews.title}','${eachNews.author.name}','${eachNews.details.slice(0, 400)}')"
+    class="row ">
+    <div class="col-md-4 p-5">
+        <img height=200px; src="${eachNews.thumbnail_url}" class="img-fluid rounded-start " alt="...">
+    </div>
+    <div class="col-md-8">
+        <div class="card-body py-5">
+            <h3 class="card-title">${eachNews.title}</h3>
+            <p class="card-text">${eachNews.details.slice(0, 300)}<strong data-bs-toggle="tooltip"
+                    data-bs-placement="top" data-bs-title="Tooltip on top" title="more read click">
+                    ...
+                </strong>
+            </p>
+
+            <div class="text-muted d-md-flex d-lg-flex text-justify justify-content-between pt-md-5 mt-md-5">
+                <div class="d-flex">
+                    <img height=45px; class="rounded-circle mx-2" src="${eachNews.author.img}">
+                    <div style="height:40px">
+                        <p style="height:50%; margin:0 2px" class="fw-bold text-dark">${eachNews.author.name}</p>
+                        <p style="height:50%; margin:0 2px">${eachNews.author.published_date}
+                        </p>
                     </div>
-                    <div class="d-flex py-3">
-                        <img height=25px; src="images/view-1.png">
-                        <strong class="px-md-2 ">${eachNews.total_view}</strong>
-                    </div>
-                    <div class="d-flex text-muted">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star-half-stroke"></i>
-                        <strong class="px-2">${eachNews.rating.number}</strong>
-                    </div>
-                    <div class="px-2 text-primary fs-2 me-3 px-3 d-none d-md-block d-lg-block">
-                        <strong>&rarr;</strong>
-                    </div>
+                </div>
+                <div class="d-flex py-3">
+                    <img height=25px; src="images/view-1.png">
+                    <strong class="px-md-2 ">${eachNews.total_view}</strong>
+                </div>
+                <div class="d-flex text-muted">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star-half-stroke"></i>
+                    <strong class="px-2">${eachNews.rating.number}</strong>
+                </div>
+                <div class="px-2 text-primary fs-2 me-3 px-3 d-none d-md-block d-lg-block">
+                    <strong>&rarr;</strong>
                 </div>
             </div>
         </div>
     </div>
-        `;
-        newsCard.appendChild(newsDiv);
+</div>
+    `;
+            newsCard.appendChild(newsDiv);
+
+        }
+
     })
     loadSpiner(false);
 }
